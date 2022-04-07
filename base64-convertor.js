67417f221c8765a016b7bba3b7e60a323750f310
Xiaolin Liu <lixiaoli@microsoft.com>
2022-04-07 17:13:11 +0800

    chore: add friendly cli instructions

--- base64-convertor.js
+++ base64-convertor.js
@@ -2,12 +2,10 @@ const imageToBase64 = require("image-to-base64");
 const chalk = require("chalk");
 const fs = require("fs");
 const path = require("path");
+const process = require("process");
 const { validImage } = require("./util");
 
 async function base64Convertor(fileOrDir) {
-  if(!fileOrDir) {
-    throw new Error(chalk.red.bold("Please give a file path or directory of images"));
-  }
   const stat = fs.lstatSync(fileOrDir);
   if (stat.isFile()) {
     const filename = path.basename(fileOrDir);
@@ -42,7 +40,7 @@ async function base64Convertor(fileOrDir) {
       if (err) {
         throw err;
       }
-      console.log(chalk.green.bold("The images is successfully saved in base64.json(current directory)"));
+      console.log(chalk.green.bold(`The images is successfully saved in ${process.cwd()}/base64.json`));
     });
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 #!/usr/bin/env node
 
-const process = require("process");
+const chalk = require("chalk");
+const yargs = require("yargs");
+const fs = require("fs");
 const base64Convertor = require("./base64-convertor");
 
-base64Convertor(process.argv[2]);
+function builder(cmd) {
+  cmd.positional("fileOrDir", {
+    describe: "file or directory",
+    coerce: (param) => {
+      fs.lstat(param, (error, stat) => {
+        if(error || !stat.isFile() && !stat.isDirectory()) {
+          throw new Error(chalk.red.bold("Please give a valid file path or directory of images"));
+        }
+      });
+      return param;
+    }
+  })
+}
+
+function handler({ fileOrDir }) {
+  base64Convertor(fileOrDir);
+}
+
+yargs.command("* <fileOrDir>", false, builder, handler).parse();
